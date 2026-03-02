714f827fbda7268b5dc117077ca66680e5775119
LakshyaMewara0025 <Lakshyamewara0025@gmail.com>
2026-03-02 18:23:13 +0530

    Lazy-loading implemented and singleton refactoring done.

--- singleton-metrics/src/com/example/metrics/MetricsLoader.java
+++ singleton-metrics/src/com/example/metrics/MetricsLoader.java
@@ -22,7 +22,7 @@ public class MetricsLoader {
         }
 
         // BROKEN: should not create a new instance
-        MetricsRegistry registry = new MetricsRegistry();
+        MetricsRegistry registry = MetricsRegistry.getInstance(); //calling the static method of metricsRegistry class
 
         for (String key : props.stringPropertyNames()) {
             String raw = props.getProperty(key, "0").trim();
